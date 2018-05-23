af0425cead239a18cef387f678842d5cc6b85118
iagofontes <iagofontes@hotmail.com>
2018-05-23 00:05:02 -0300

    commit final

---
+++ Exercicio_ChaveComposta/src/br/com/fiap/pk/PedidosPK.java
@@ -0,0 +1,55 @@
+package br.com.fiap.pk;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class PedidosPK implements Serializable {
+
+	private static final Long serialVersionUID=1L;
+	
+	@Column(name="NUMPEDIDO")
+	private int codigo;
+	
+	@Column(name="CATEGORIA")
+	private String categoria;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public static Long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof PedidosPK) {
+			PedidosPK pk = (PedidosPK) o;
+			if(this.getCodigo() != pk.getCodigo()) {
+				return false;
+			}
+			if(this.getCategoria() != pk.getCategoria()) {
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.getCategoria().hashCode() + this.getCodigo();
+	}
+	
+}
